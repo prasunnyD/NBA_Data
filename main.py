12dba17fe9f6e9eaa5638fcf1fd727a704ca8727
prasunnyD <prasunnyD@github.com>
2023-12-23 22:39:18 -0500

    working linear regression model for pts

--- main.py
+++ main.py
@@ -4,67 +4,26 @@ from model import *
 import matplotlib.pyplot as plt 
 import pandas as pd
 import time
+import joblib
 
 a_edwards = Player('Anthony Edwards','Minnesota')
 timberwolves = Team('Minnesota')
 def_stats = timberwolves.get_team_opp_efga()
 def_stats.to_csv("def_team_stats.csv")
-# a_edwards_df = a_edwards.player_career_boxscore()
-# print(a_edwards_df)
-# a_edwards_df.to_csv('ant_edwards.csv')
-pts_df = a_edwards.player_stat('PTS')
-# pts_df.to_csv("edwards_pts.csv")
-opps = pts_df['OPPONENT'] 
-opponents_efga_list = list()
-opponents_free_throw_rate_list = list()
-opponents_orebounding_pct_list = list()
-team_dict = dict()
-# for opp in opps:
-#     if opp in team_dict:
-#         team_df = team_dict[opp]
-#     else:
-#         team = abrv_team_dict(opp)
-#         team_df = team.get_team_opp_efga()
-#         team_dict.update({opp:team_df})
-#     time.sleep(1)
-#     opponents_efga_list.append(team_df['OPP_EFG_PCT'])
-#     opponents_free_throw_rate_list.append(team_df['OPP_FTA_RATE'])
-#     opponents_orebounding_pct_list.append(team_df['OPP_OREB_PCT'])
-                               
-      
-# opponents_efga_list = [abrv_team_dict(opp).get_team_opp_efga() for opp in opps]
-# pts_df["OPP_EFG_PCT"]= opponents_efga_list
-# pts_df["OPP_FTA_RATE"]= opponents_free_throw_rate_list
-# pts_df["OPP_OREB_PCT"]= opponents_orebounding_pct_list
-# pts_df.to_csv("edwards_pts.csv")
-# stats = pd.read_csv("edwards_pts.csv")
-# edwards_df= pts_df.drop(columns=['SEASON_ID','OPPONENT','GAME_DATE','LOCATION'])
-# home_away = pd.get_dummies(edwards_df, columns=['LOCATION'], dtype=int)
-# print(home_away)
-# x_train,x_test,y_train,y_test = build_TrainTest(edwards_df,'PTS')
-# model,y_pred = RunLinearModel(x_train,y_train,x_test,y_test)
-# plt.scatter(y_pred ,y_test)
-# plt.xlabel('opp fga')
-# plt.ylabel('predicted points')
-# plt.show()
+X_test = [[0.559,0.272,0.263,38]]
+predict_result("edwards_model.sav")
 
-def get_minutes(player):
-    min_df = player.player_minutes()
-    return min_df
 
-def get_points(player):
-    pts_df = player.player_stat('PTS')
-    return pts_df
+def make_player_csv(player, csv_name : str, stat : str):
+   points_df = player.player_stat(stat)
+   stats_df = opp_data(points_df)
+   minutes_df =  player.player_minutes()
+   stats_df['MINUTES'] = minutes_df['prev_3_avg']
+   stats_df.dropna(inplace=True)
+   stats_df.to_csv(csv_name, index = False)
 
-def make_player_csv(player, csv_name):
-   minutes_df = get_minutes(player)
-   points_df = get_points(player)
-   points_df['MINUTES'] = minutes_df['prev_3_avg']
-   points_df.dropna(inplace=True)
-   points_df.to_csv(csv_name)
-
-def linear_regression():
-    stats = pd.read_csv("edwards_pts.csv")
+def linear_regression(csv_name : str):
+    stats = pd.read_csv(csv_name)
     print(stats)
     player_df= stats.drop(columns=['SEASON_ID','OPPONENT','GAME_DATE','LOCATION'])
     x_train,x_test,y_train,y_test = build_TrainTest(player_df,'PTS')
@@ -74,4 +33,38 @@ def linear_regression():
     plt.ylabel('predicted points')
     plt.show()
 
-linear_regression()
+    return model
+
+def opp_data(df):
+    opponents_efga_list = list()
+    opponents_free_throw_rate_list = list()
+    opponents_orebounding_pct_list = list()
+    team_dict = dict()
+    opps = df['OPPONENT'] 
+    for opp in opps:
+        if opp in team_dict:
+            team_df = team_dict[opp]
+        else:
+            team = abrv_team_dict(opp)
+            team_df = team.get_team_opp_efga()
+            team_dict.update({opp:team_df})
+        time.sleep(1)
+        opponents_efga_list.append(team_df['OPP_EFG_PCT'][0])
+        opponents_free_throw_rate_list.append(team_df['OPP_FTA_RATE'][0])
+        opponents_orebounding_pct_list.append(team_df['OPP_OREB_PCT'][0])
+
+    df["OPP_EFG_PCT"]= opponents_efga_list
+    df["OPP_FTA_RATE"]= opponents_free_throw_rate_list
+    df["OPP_OREB_PCT"]= opponents_orebounding_pct_list
+
+    return df
+
+def create_model_from_scratch(csv_name : str, model_filename : str):
+    make_player_csv(a_edwards, csv_name)
+    model =linear_regression(csv_name)
+    joblib.dump(model,model_filename)
+
+def predict_result(model_filename : str, X_test: list):
+    loaded_model = joblib.load(model_filename)
+    result = loaded_model.predict(X_test)
+    print(result)
